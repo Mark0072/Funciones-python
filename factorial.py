497632da5f13cc2ab9646c2c9d07f959bae7a4b4
Mark00071 <leonelantoniobidodelorbe@gmail.com>
2025-07-28 16:35:19 -0400

    first commit

--- factorial.py
+++ factorial.py
@@ -1,11 +1,11 @@
 def factorial(n):
     result = 1
-    # Iterate from 1 up to n (inclusive)
+   
     for i in range(1, n + 1):
-        result *= i  # Multiply result by the current number
+        result *= i  
     return result
 
-# Example usage:
+
 print(factorial(5))
 
 
